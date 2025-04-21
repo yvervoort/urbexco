ceae7d4c4541f44529600e625bc65e173e3398e2
yvervoort <47427104+yvervoort@users.noreply.github.com>
2025-04-21 18:09:21 +0200

    Update script.js

--- linktree/dev/script.js
+++ linktree/dev/script.js
@@ -13,13 +13,17 @@ document.getElementById("newsletter-form").addEventListener("submit", function (
 
   // Prepare the data to send
   const data = {
+    date: new Date().toISOString(),
     email: email,
     consent: consent
   };
 
+  console.log(data)
+
   // Send the data to Google Apps Script
   fetch('https://script.google.com/macros/s/AKfycbykiURy21_XBfx3-r7Muqx2yOIKul-mzfzKCzgy7SMsJ2bfTewnO8vdNNuBN7tApXbFrA/exec', {
     method: 'POST',
+    mode: 'cors',
     headers: {
       'Content-Type': 'application/json',
     },
